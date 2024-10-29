fd108ec1f55c81cec1fc3724b9d81304c64f1880
Boris Milchenko <b.milchenko@xlab-game.com>
2024-10-29 18:03:35 +0300

    Adds simple PlayerController

---
+++ Assets/Scripts/Golf/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Compression;
+using UnityEngine;
+
+namespace Golf {
+    public class PlayerController : MonoBehaviour
+    {
+        public Transform stick;
+        public float maxAngle = 30;
+
+        private void Update()
+        {
+            var angle = stick.localEulerAngles;
+            if (Input.GetMouseButton(0))
+            {
+                angle.z = maxAngle;
+            } else
+            {
+                angle.z = -maxAngle;
+            }
+            stick.localEulerAngles = angle;
+        }
+    }
+}

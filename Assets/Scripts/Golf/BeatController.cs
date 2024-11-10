4d30a485313aba7e031bc09fde06efa8a24a27cc
BoreyTheTourist <borey_the_tourist@mail.ru>
2024-11-10 15:33:33 +0300

    Fixes late player return bug
    When ball exits, BeatController must count that there's maybe player's attempt to hit the ball that will we succeeded

--- Assets/Scripts/Golf/BeatController.cs
+++ Assets/Scripts/Golf/BeatController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -12,6 +13,8 @@ namespace Golf {
         [SerializeField] private bool m_deviationZ;
         private GameObject m_ball;
         private bool m_wasHit = false;
+        private bool m_isReturn = false;
+        private Coroutine m_ballExit;
 
         public event System.Action OnBallEnter;
         public event System.Action<bool> OnBallExit;
@@ -20,7 +23,9 @@ namespace Golf {
         {
             if (m_ballPrefab) {
                 var obj = Instantiate(m_ballPrefab, transform.position, transform.rotation);
-                Beat(obj);
+                if (obj.TryGetComponent<Rigidbody>(out var rb)) {
+                    Beat(rb);
+                }
                 return obj;
             }
             return null;
@@ -31,9 +36,11 @@ namespace Golf {
         /// </summary>
         public void Return()
         {
-            if (m_ball) {
-                Beat(m_ball);
+            m_isReturn = true;
+            if (m_ball && m_ball.TryGetComponent<Rigidbody>(out var rb)) {
+                Beat(rb);
             }
+            m_isReturn = false;
         }
 
         /// <summary>
@@ -42,49 +49,57 @@ namespace Golf {
         /// <returns>If ball in specified half normalized position ([-1, 1])</returns>
         public Vector2? Return(Half half)
         {
-            if (m_ball) {
+            m_isReturn = true;
+            if (m_ball && m_ball.TryGetComponent<Rigidbody>(out var rb)) {
                 bool isHitLeft = half == Half.Left;
                 var col = GetComponent<Collider>();
                 var ballPosition = m_ball.transform.position - col.bounds.center;
                 bool isBallCenter = Mathf.Abs(ballPosition.x) < m_ball.GetComponent<Collider>().bounds.max.x;
                 if (isBallCenter || isHitLeft ^ ballPosition.x > 0) {
-                    Beat(m_ball);
+                    Beat(rb);
                     ballPosition.x /= col.bounds.extents.x;
                     ballPosition.y /= col.bounds.extents.y;
                     return ballPosition;
                 }
             }
+            m_isReturn = false;
             return null;
         }
 
-        private void Beat(GameObject obj)
+        private void Beat(Rigidbody rb)
         {
             m_wasHit = true;
-            if (obj.TryGetComponent<Rigidbody>(out var rb)) {
-                var target = m_target.bounds.center;
-                var dev = m_target.bounds.size * 0.5f;
-                if (m_deviationX) {
-                    target.x += Random.Range(-dev.x, maxInclusive: dev.x);
-                }
-                if (m_deviationY) {
-                    target.y += Random.Range(-dev.y, maxInclusive: dev.y);
-                }
-                if (m_deviationZ) {
-                    target.z += Random.Range(-dev.z, maxInclusive: dev.z);
-                }
-                rb.velocity = Vector3.zero;
-                rb.AddForce((target - obj.transform.position).normalized * m_power, ForceMode.Impulse);
+            var target = m_target.bounds.center;
+            var dev = m_target.bounds.size * 0.5f;
+            if (m_deviationX) {
+                target.x += Random.Range(-dev.x, maxInclusive: dev.x);
             }
+            if (m_deviationY) {
+                target.y += Random.Range(-dev.y, maxInclusive: dev.y);
+            }
+            if (m_deviationZ) {
+                target.z += Random.Range(-dev.z, maxInclusive: dev.z);
+            }
+            rb.velocity = (target - rb.gameObject.transform.position).normalized * m_power;
         }
 
         private void OnTriggerEnter(Collider col)
         {
             m_ball = col.gameObject;
             OnBallEnter?.Invoke();
+            if (m_ballExit != null) {
+                StopCoroutine(m_ballExit);
+            }
         }
 
         private void OnTriggerExit(Collider col)
         {
+            m_ballExit = StartCoroutine(BallExit());
+        }
+
+        private IEnumerator BallExit()
+        {
+            yield return new WaitUntil(() => !m_isReturn);
             m_ball = null;
             OnBallExit?.Invoke(m_wasHit);
             m_wasHit = false;

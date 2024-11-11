4f717b316c25f630570ea10c333c95a81578fcc0
BoreyTheTourist <borey_the_tourist@mail.ru>
2024-11-11 22:23:39 +0300

    Fixes player late return bug
    Again ;)

--- Assets/Scripts/Golf/BeatController.cs
+++ Assets/Scripts/Golf/BeatController.cs
@@ -5,23 +5,29 @@ using UnityEngine.UIElements;
 namespace Golf {
     public class BeatController : MonoBehaviour
     {
-        [SerializeField] private Collider m_target;
+        [SerializeField] private BeatController m_target;
+        [SerializeField] private Transform m_servePoint;
         [SerializeField] private GameObject m_ballPrefab;
         [SerializeField] private bool m_deviationX = true;
         [SerializeField] private bool m_deviationY;
         [SerializeField] private bool m_deviationZ;
         private GameObject m_ball;
         private bool m_wasHit = false;
-        private bool m_isReturn = false;
-        private Coroutine m_ballExit;
 
         public event System.Action OnBallEnter;
         public event System.Action OnBallExit;
 
+        private void Start()
+        {
+            if (!m_servePoint) {
+                m_servePoint = transform;
+            }
+        }
+
         public GameObject Serve()
         {
             if (m_ballPrefab) {
-                var obj = Instantiate(m_ballPrefab, transform.position, transform.rotation);
+                var obj = Instantiate(m_ballPrefab, m_servePoint.position, Quaternion.identity);
                 if (obj.TryGetComponent<Rigidbody>(out var rb)) {
                     Beat(rb);
                 }
@@ -30,16 +36,19 @@ namespace Golf {
             return null;
         }
 
+        public void Receive()
+        {
+            m_wasHit = false;
+        }
+
         /// <summary>
         /// Just hits ball
         /// </summary>
         public void Return()
         {
-            m_isReturn = true;
             if (m_ball && m_ball.TryGetComponent<Rigidbody>(out var rb)) {
                 Beat(rb);
             }
-            m_isReturn = false;
         }
 
         /// <summary>
@@ -48,8 +57,6 @@ namespace Golf {
         /// <returns>If ball in specified half normalized position ([-1, 1])</returns>
         public Vector2? Return(Half half)
         {
-            m_isReturn = true;
-            Vector2? res = null;
             if (m_ball && m_ball.TryGetComponent<Rigidbody>(out var rb)) {
                 bool isHitLeft = half == Half.Left;
                 var col = GetComponent<Collider>();
@@ -59,21 +66,18 @@ namespace Golf {
                     Beat(rb);
                     ballPosition.x /= col.bounds.extents.x;
                     ballPosition.y /= col.bounds.extents.y;
-                    res = ballPosition;
+                    return ballPosition;
                 }
             }
-            m_isReturn = false;
-            return res;
+            return null;
         }
 
         private void Beat(Rigidbody rb)
         {
             m_wasHit = true;
-            if (m_ballExit != null) {
-                StopCoroutine(m_ballExit);
-            }
-            var target = m_target.bounds.center;
-            var dev = m_target.bounds.size * 0.5f;
+            m_target.Receive();
+            var target = m_target.GetComponent<Collider>().bounds.center;
+            var dev = m_target.GetComponent<Collider>().bounds.size * 0.5f;
             if (m_deviationX) {
                 target.x += Random.Range(-dev.x, maxInclusive: dev.x);
             }
@@ -91,26 +95,14 @@ namespace Golf {
         {
             m_ball = col.gameObject;
             OnBallEnter?.Invoke();
-            if (m_ballExit != null) {
-                StopCoroutine(m_ballExit);
-            }
         }
 
         private void OnTriggerExit(Collider col)
         {
-            m_ballExit = StartCoroutine(BallExit());
-        }
-
-        private IEnumerator BallExit()
-        {
-            yield return new WaitForEndOfFrame();
-            yield return new WaitForEndOfFrame();
-            yield return new WaitUntil(() => !m_isReturn);
             if (!m_wasHit) {
                 m_ball = null;
                 OnBallExit?.Invoke();
             }
-            m_wasHit = false;
         }
     }
 }
